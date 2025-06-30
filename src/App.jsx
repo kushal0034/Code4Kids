fa606d524af29f64ea06282dce019b059060f9ef
kushal0034 <kushalvenkatchowdarymikkilineni@my.unt.edu>
2025-06-30 18:35:53 +0000

    Adding the Routes in the root javascript file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Routes ,Route} from "react-router-dom"
 import LandingPage from "./pages/LandingPage"
-import SigninPage from "./pages/SigninPage"
+import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
+import StudentDashboard from './pages/StudentDashboardPage'
+import TeacherDashboardPage from './pages/TeacherDashboardPage'
+import ParentDashboard from './pages/ParentDashboardPage'
+import WorldsPage from './pages/WorldsPage'
+import LevelOnePage from './pages/LevelOnePage'
+import LevelTwoGame from "./pages/LevelTwoGame"
 
 function App() {
 
@@ -12,7 +17,14 @@ function App() {
     <>
     <Routes>
       <Route path="/" element={<LandingPage/>} />
-      <Route path="/login" element={<SigninPage/>} />
+      <Route path="/login" element={<LoginPage/>} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path='/student-dashboard' element={<StudentDashboard />} />
+      <Route path="/teacher-dashboard" element={<TeacherDashboardPage />} />
+      <Route path='/parent-dashboard' element={<ParentDashboard />} />
+      <Route path="/worlds" element={<WorldsPage />} />
+      <Route path="/level-1" element={<LevelOnePage />} />
+      <Route path="/level-2" element={<LevelTwoGame />} />
     </Routes>
     </>
   )
--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBuZhroBZxQjPa90g4e7N3uyEZB3ylORXo",
-  authDomain: "code4kids-6d0b8.firebaseapp.com",
-  projectId: "code4kids-6d0b8",
-  storageBucket: "code4kids-6d0b8.firebasestorage.app",
-  messagingSenderId: "890917379223",
-  appId: "1:890917379223:web:1907914a205d144ec79dd2",
-  measurementId: "G-ZBHJ6SS4Z2"
+  apiKey: "AIzaSyC8DukbBkQiMJAPV84pLnhLVUx_NXE90Ko",
+  authDomain: "code4kids-895f4.firebaseapp.com",
+  projectId: "code4kids-895f4",
+  storageBucket: "code4kids-895f4.firebasestorage.app",
+  messagingSenderId: "599702627168",
+  appId: "1:599702627168:web:f1d6b7e7799c836523f405",
+  measurementId: "G-KMYGDT67TJ"
 };
 
 // Initialize Firebase
